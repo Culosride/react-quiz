fa7dd574f5bc7804f06ef21f8210b5c922906d75
Andrea Spiga <drespriz@gmail.com>
2022-09-15 16:55:49 +0200

    generates answers in random order

--- app/components/Quiz.jsx
+++ app/components/Quiz.jsx
@@ -1,15 +1,25 @@
 export default function Quiz(props) {
 
   const answers = (props.wrongAnswer).map(answer => {
-    return <p className="quiz-answer">{answer}</p>
+    return answer
   })
+
+  answers.push(props.answer)
+
+  // generate randomly ordered array of answers everytime the component is rendered
+
+  let allAnswers = answers.sort(() => Math.random() - 0.5)
+
+  allAnswers = allAnswers.map((answer, i) => {
+    return <p key={i} id={i} className="quiz-answer">{answer}</p>
+  })
+
 
   return (
     <div className="quiz-container">
       <h3 className="quiz-qst">{props.question}</h3>
       <div className="quiz-answer-container">
-        {answers}
-        <p className="quiz-answer">{props.answer}</p>
+        {allAnswers}
       </div>
       <hr />
     </div>
